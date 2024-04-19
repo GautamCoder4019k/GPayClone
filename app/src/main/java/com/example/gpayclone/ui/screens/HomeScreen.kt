78c6d674055c973a22043a728800d4e0f69046bb
Gautam Shorewala <123801344+GautamCoder4019k@users.noreply.github.com>
2024-04-19 16:49:25 +0530

    --updated the project

--- app/src/main/java/com/example/gpayclone/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gpayclone/ui/screens/HomeScreen.kt
@@ -3,7 +3,9 @@ package com.example.gpayclone.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -15,11 +17,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gpayclone.presenter.signIn.UserData
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -28,13 +37,21 @@ fun HomeScreen(userData: UserData = UserData("", "", "")) {
     LaunchedEffect(Unit) {
         viewModel.addUser(userData.userId, userData.username ?: "")
     }
+    var balance by remember {
+        mutableStateOf("")
+    }
+
+    val scope = rememberCoroutineScope()
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
         Card(modifier = Modifier.padding(16.dp)) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(16.dp)) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
                 Icon(
                     imageVector = Icons.Default.AccountCircle,
                     contentDescription = null,
@@ -43,14 +60,22 @@ fun HomeScreen(userData: UserData = UserData("", "", "")) {
                 Text(text = userData.username!!)
                 Row {
 
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = { viewModel.increaseBalance(userData.userId,1000) }) {
                         Text(text = "Add Amount")
                     }
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = {
+                        scope.launch {
+                            balance=viewModel.fetchBalance(userData.userId)
+                        }
+                    }
+                    ) {
                         Text(text = "Check Balance")
                     }
                 }
             }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Your Balance ${balance}")
     }
 }
--- app/src/main/java/com/example/gpayclone/ui/screens/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/gpayclone/ui/screens/HomeScreenViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.gpayclone.ui.screens
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.tasks.await
 
 class HomeScreenViewModel : ViewModel() {
 
+    val balance =MutableStateFlow("")
+    val _balance =balance.asStateFlow()
 
     fun checkUserDocumentExists(
         userId: String,
@@ -21,7 +27,7 @@ class HomeScreenViewModel : ViewModel() {
                 onComplete(false)
             }
     }
-    fun increaseBalance(userId: String, amount: Int, firestore: FirebaseFirestore) {
+    fun increaseBalance(userId: String, amount: Int, firestore: FirebaseFirestore=FirebaseFirestore.getInstance()) {
         // Update the balance field in the user document by incrementing it by the given amount
         val userRef = firestore.collection("users").document(userId)
         userRef.update("balance", amount)
@@ -51,9 +57,31 @@ class HomeScreenViewModel : ViewModel() {
             }
     }
 
+    suspend fun fetchBalance(userId: String, firestore: FirebaseFirestore=FirebaseFirestore.getInstance()):String {
+        // Get a reference to the user document
+        val userRef = firestore.collection("users").document(userId)
+
+        try {
+            // Get the user document from Firestore
+            val snapshot = userRef.get().await()
+
+            // Check if the user document exists
+            if (snapshot.exists()) {
+                // Extract and return the balance from the user document
+               balance.value= snapshot.getLong("balance").toString()
+                return snapshot.getLong("balance").toString()
+            } else {
+                // User document does not exist
+                throw IllegalStateException("User document not found for ID: $userId")
+            }
+        } catch (e: Exception) {
+            // Handle exceptions
+            throw IllegalStateException("Failed to fetch balance for user ID: $userId", e)
+        }
+    }
+
     // Usage
     // Replace with the actual user ID
-
 
     // Check if the user document exists
     fun addUser(userId: String,userName: String) {
